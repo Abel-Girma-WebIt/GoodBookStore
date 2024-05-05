c5cc2a35badf176fb3783256213a15bcde264e5b
Abel-Girma-WebIt <gedamu0918@gmail.com>
2024-05-05 17:53:52 -0400

    Azx

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 
   
 //   Enable CORS for all routes
-  app.use(cors({ origin: 'https://good-book-store-6k0nj753r-abel-girma-webits-projects.vercel.app', credentials: true ,
+  app.use(cors({ origin: 'https://good-book-store-fe.vercel.app', credentials: true ,
   methods: 'GET,POST,PUT,DELETE'
    }));
 
