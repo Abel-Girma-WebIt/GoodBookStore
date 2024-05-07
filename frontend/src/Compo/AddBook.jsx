08484b17b8fcd2aca05fe71aca362a1f89b01fe7
Abel-Girma-WebIt <146472103+Abel-Girma-WebIt@users.noreply.github.com>
2024-05-07 10:13:41 -0400

    Update AddBook.jsx

--- frontend/src/Compo/AddBook.jsx
+++ frontend/src/Compo/AddBook.jsx
@@ -33,7 +33,7 @@ function postBookButton(e){
 
     e.preventDefault();
 
-    axios.post('https://bookstorebackend-jzt9hayz9-abel-girma-webits-projects.vercel.app/books/addbooks' , newBook)
+    axios.post('https://bookstorebackend-nsb32tta8-abel-girma-webits-projects.vercel.app/books/addbooks' , newBook)
     .then((res)=>{
                if(res.data.valid){
                 setNewBook({title:"",author :"",year:"",image:"",desc:""});
