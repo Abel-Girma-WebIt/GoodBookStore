3d5dce9cffcc714e1bbee1c3eed924c9c479c1d1
Abel-Girma-WebIt <gedamu0918@gmail.com>
2024-05-07 22:24:40 -0400

    adszcsxc

--- frontend/src/Compo/ViewAllBooks.jsx
+++ frontend/src/Compo/ViewAllBooks.jsx
@@ -28,7 +28,7 @@ axios.get('https://good-book-store-be.vercel.app/books/all-books')
         console.log(`Error fetching user data: ${err}`);
         Navigate('/user/login');})
 
-},[0])
+},[])
 
 
 function addInputeData(e){
